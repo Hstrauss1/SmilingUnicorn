71df26cc1a50b524ba83b94455da3a752d5264c3
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2026-02-28 22:04:36 -0800

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#2d2d2d] text-[#b8b3a3] py-12 px-6 lg:px-8">
+    <footer className="bg-transparent text-[#b8b3a3] py-12 px-6 lg:px-8">
       <div className="mx-auto max-w-7xl">
         <div className="border-t border-[#4a4a4a] pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
           <p className="text-[#888378]">
